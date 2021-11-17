e99bdc82fa50d28f0fe1efbdd96018a524ce94aa
Leonardo Henrique Guimaraes <leonardo.guimaraes@direcional.com.br>
2021-11-17 18:49:54 -0300

    Adding models

--- Implementacao/lab/coin_system/models.py
+++ Implementacao/lab/coin_system/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Alunos:
+    nome = ''
+    email = ''
+    CPF = ''
+    RG = ''
+    endereco = ''
+    instituicaoEnsino = ''
+    curso = ''
+
+    def get_extrato_conta():
+        pass
+
+class Professores:
+    nome = ''
+    CPF = ''
+    departamento = ''
+    instituicaoEnsino = ''
+
+    def get_extrato_conta():
+        pass
+    
+class EmpresasParceiras:
+    pass
+
+class Moedas:
+    moedas_professores_semestre = 1000
+    
+
+# Conta, Saldo, 
